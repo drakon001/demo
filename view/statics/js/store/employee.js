7cb673069182e16f327978150afd2abce653f9fa
drakon001 <dr.drakon@bk.ru>
2016-10-17 14:04:31 +0300

    Create employee.js

---
+++ view/statics/js/store/employee.js
@@ -0,0 +1,12 @@
+$Q.define('store.employee',{
+    extend:'Backbone.Collection',
+    model: $Q.Role('models.employee'),
+    localStorage: new Backbone.LocalStorage("employee-backbone"), 
+    initialize: function() {
+      var sync =function(model){          model.save();              };
+       this.on("add",     sync);
+       this.on("change",  sync);
+       this.fetch();
+    }
+  });
+  
